613428f9e3407321885cffd3e3582bd0144d7e2c
Tony <peng2251@163.com>
2017-01-20 22:32:53 +0800

    Add files via upload
    add new files

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,1 +1,33 @@
 
+public class Board {
+    public Board(int[][] blocks) {          // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    } 
+    public int dimension() {                // board dimension n
+        return 0;
+    }
+    public int hamming()  {                 // number of blocks out of place
+        return 0;
+    }
+    public int manhattan() {                // sum of Manhattan distances between blocks and goal
+        return 0;
+    }
+    public boolean isGoal() {               // is this board the goal board?
+        return false;
+    }
+    public Board twin()   {                 // a board that is obtained by exchanging any pair of blocks
+        return null;
+    }
+    public boolean equals(Object y)  {      // does this board equal y?
+        return false;
+    }
+    public Iterable<Board> neighbors() {    // all neighboring boards
+        return null;
+    }
+    public String toString()   {            // string representation of this board (in the output format specified below)
+        return null;
+    }
+    public static void main(String[] args) {// unit tests (not graded)
+    
+    }
+}
---
+++ 8puzzle/Solver.java
@@ -0,0 +1,18 @@
+
+public class Solver {
+    public Solver(Board initial) {          // find a solution to the initial board (using the A* algorithm)
+    
+    }
+    public boolean isSolvable() {           // is the initial board solvable?
+        return false;
+    }
+    public int moves() {                    // min number of moves to solve initial board; -1 if unsolvable
+        return 0;
+    }
+    public Iterable<Board> solution() {     // sequence of boards in a shortest solution; null if unsolvable
+        return null;
+    }
+    public static void main(String[] args) {// solve a slider puzzle (given below)
+        
+    }
+}
